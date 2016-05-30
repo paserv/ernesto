5d7260e5b3fe4313241f89638fd8f7a29e65615e
DonPablo <paserv@gmail.com>
2016-05-30 14:41:41 +0200

    admin page

---
+++ ConsulenzaLegale/admin/footer.php
@@ -0,0 +1,5 @@
+<footer class="page-footer">
+	<div class="footer-copyright container-footer" style="padding:0 20px 0 20px">
+			<div class="blue-text text-lighten-4 right"><h5>Pannello di Controllo</h5></div>
+		</div>
+</footer>
---
+++ ConsulenzaLegale/admin/header.php
@@ -0,0 +1,12 @@
+<header>
+<div class="navbar-fixed">
+	<nav>
+		<div class="nav-wrapper">
+			<ul class="left hide-on-med-and-down">
+				<li><a class="tooltipped" data-position="bottom" data-delay="100" data-tooltip="Home Page" href="../index.php" ><img src="../public/img/logo_white_pic.png"></a></li>
+			</ul>
+			<a href="#" class="brand-logo">Pannello di Controllo</a>
+		</div>
+	</nav>
+</div>
+</header>
