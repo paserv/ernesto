934399e362ace73ce4ed1aae57a0ecd88336e19a
DonPablo <paserv@gmail.com>
2016-06-06 17:13:29 +0200

    head

---
+++ ConsulenzaLegale/head.php
@@ -0,0 +1,38 @@
+			<title>Ricorsi & Controricorsi</title>
+			<meta charset="utf-8">
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+			<meta name="description" content="Siamo un team di giuristi, con competenze in diverse materie, che intendono offrire dei pareri GRATUITI al cittadino in merito alla possibilità ed alla sussistenza di presupposti per presentare ricorso avverso un Verbale di accertamento per violazioni al Codice della Strada, una Cartella Esattoriale o contro una Sanzione Tributaria." />
+			<meta name="google" content="notranslate" />
+			<meta name="msapplication-TileColor" content="#ffffff">
+			<meta name="msapplication-TileImage" content="public/icons/ms-icon-144x144.png">
+			<meta name="theme-color" content="#ffffff">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+			<meta property="og:title" content="Ricorsi & Controricorsi">
+			<meta property="og:image" content="http://www.ricorsiecontroricorsi.it/public/img/logo.png">
+			<meta property="og:site_name" content="Ricorsi & Controricorsi">
+			<meta property="og:description" content="Siamo un team di giuristi, con competenze in diverse materie, che intendono offrire dei pareri GRATUITI al cittadino in merito alla possibilità ed alla sussistenza di presupposti per presentare ricorso avverso un Verbale di accertamento per violazioni al Codice della Strada, una Cartella Esattoriale o contro una Sanzione Tributaria.">
+			<meta property="og:locale" content="it_IT">
+			<link rel="icon" href="favicon.ico" />
+			<link type="text/css" rel="stylesheet" href="public/css/materialize.min.css"  media="screen,projection"/>
+			<link type="text/css" rel="stylesheet" href="public/css/jquery-ui.css">
+			<link type="text/css" rel="stylesheet" href="public/css/site.css" />
+			<link rel="apple-touch-icon" sizes="57x57" href="public/icons/apple-icon-57x57.png">
+			<link rel="apple-touch-icon" sizes="60x60" href="public/icons/apple-icon-60x60.png">
+			<link rel="apple-touch-icon" sizes="72x72" href="public/icons/apple-icon-72x72.png">
+			<link rel="apple-touch-icon" sizes="76x76" href="public/icons/apple-icon-76x76.png">
+			<link rel="apple-touch-icon" sizes="114x114" href="public/icons/apple-icon-114x114.png">
+			<link rel="apple-touch-icon" sizes="120x120" href="public/icons/apple-icon-120x120.png">
+			<link rel="apple-touch-icon" sizes="144x144" href="public/icons/apple-icon-144x144.png">
+			<link rel="apple-touch-icon" sizes="152x152" href="public/icons/apple-icon-152x152.png">
+			<link rel="apple-touch-icon" sizes="180x180" href="public/icons/apple-icon-180x180.png">
+			<link rel="icon" type="image/png" sizes="192x192"  href="public/icons/android-icon-192x192.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="public/icons/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="96x96" href="public/icons/favicon-96x96.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="public/icons/favicon-16x16.png">
+			<link rel="manifest" href="/manifest.json">
+			<link rel="mask-icon" href="public/icons/safari-pinned-tab.svg" color="#5bbad5">
+			<link rel="shortcut icon" href="public/icons/favicon.ico" type="image/x-icon">
+			<script type="text/javascript" src="public/js/jquery-2.1.3.min.js"></script>
+			<script type="text/javascript" src="public/js/jquery-ui-1.11.4.js"></script>
+			<script type="text/javascript" src="public/js/materialize.min.js"></script>
