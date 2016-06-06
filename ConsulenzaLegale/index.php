b3542b53ab324450f3bace4b196085873eb94900
DonPablo <paserv@gmail.com>
2016-06-06 17:13:14 +0200

    modifiche metadati

--- ConsulenzaLegale/index.php
+++ ConsulenzaLegale/index.php
@@ -1,44 +1,7 @@
-<?php 
-require_once 'autoload.php';
-autoload();
-?>
 <!DOCTYPE html>
 	<html>
 		<head>
-			<title>Ricorso Verbali C.d.S. - Ernesto Marzano</title>
-			<link rel="icon" href="favicon.ico" />
-			<link type="text/css" rel="stylesheet" href="public/css/materialize.min.css"  media="screen,projection"/>
-			<link type="text/css" rel="stylesheet" href="public/css/jquery-ui.css">
-			<link type="text/css" rel="stylesheet" href="public/css/site.css" />
-			<link rel="apple-touch-icon" sizes="57x57" href="public/icons/apple-icon-57x57.png">
-			<link rel="apple-touch-icon" sizes="60x60" href="public/icons/apple-icon-60x60.png">
-			<link rel="apple-touch-icon" sizes="72x72" href="public/icons/apple-icon-72x72.png">
-			<link rel="apple-touch-icon" sizes="76x76" href="public/icons/apple-icon-76x76.png">
-			<link rel="apple-touch-icon" sizes="114x114" href="public/icons/apple-icon-114x114.png">
-			<link rel="apple-touch-icon" sizes="120x120" href="public/icons/apple-icon-120x120.png">
-			<link rel="apple-touch-icon" sizes="144x144" href="public/icons/apple-icon-144x144.png">
-			<link rel="apple-touch-icon" sizes="152x152" href="public/icons/apple-icon-152x152.png">
-			<link rel="apple-touch-icon" sizes="180x180" href="public/icons/apple-icon-180x180.png">
-			<link rel="icon" type="image/png" sizes="192x192"  href="public/icons/android-icon-192x192.png">
-			<link rel="icon" type="image/png" sizes="32x32" href="public/icons/favicon-32x32.png">
-			<link rel="icon" type="image/png" sizes="96x96" href="public/icons/favicon-96x96.png">
-			<link rel="icon" type="image/png" sizes="16x16" href="public/icons/favicon-16x16.png">
-			<link rel="manifest" href="/manifest.json">
-			<link rel="mask-icon" href="public/icons/safari-pinned-tab.svg" color="#5bbad5">
-			<link rel="shortcut icon" href="public/icons/favicon.ico" type="image/x-icon">
-			<meta name="msapplication-TileColor" content="#ffffff">
-			<meta name="msapplication-TileImage" content="public/icons/ms-icon-144x144.png">
-			<meta name="theme-color" content="#ffffff">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-			<meta property="og:title" content="Ricorso Verbali C.d.S.">
-			<meta property="og:image" content="http://www.ricorsicodicedellastrada.com/public/img/logo.png">
-			<meta property="og:site_name" content="Ricorso Verbali C.d.S.">
-			<meta property="og:description" content="Ricorso Verbali C.d.S. - Richiedi un parere GRATUITO!!!">
-			<meta property="og:locale" content="it_IT">
-			<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-			<script type="text/javascript" src="public/js/jquery-2.1.3.min.js"></script>
-			<script type="text/javascript" src="public/js/jquery-ui-1.11.4.js"></script>
-			<script type="text/javascript" src="public/js/materialize.min.js"></script>
+			<?php include 'head.php' ?>
 		</head>
 	
 	<body>
